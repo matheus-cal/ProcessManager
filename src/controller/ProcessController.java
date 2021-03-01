2c32ba8ea179e578ca34d7c413feac35bd28e255
semantix <semantix@NTBSTX6362>
2021-03-01 13:04:59 -0300

    os, taskList, processKillerPid, processKillerName methods creation for Linux.

--- src/controller/ProcessController.java
+++ src/controller/ProcessController.java
@@ -1,5 +1,68 @@
 package controller;
 
-public class ProcessController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class ProcessController {
+	public ProcessController() {
+		super();
+	}
+	
+	public String os() {
+		String os = System.getProperty("os.name");
+		
+		return os;
+	}
+	
+	public void taskList(String os) {
+		if (os.contains("Linux")) {
+			String cmd = "ps -aux";
+			try {
+				Process p = Runtime.getRuntime().exec(cmd);
+				InputStream flow = p.getInputStream();
+				InputStreamReader reader = new InputStreamReader(flow);
+				BufferedReader buffer = new BufferedReader(reader);
+				String line = buffer.readLine();
+				
+				while (line != null) {
+					System.out.println(line);
+					line = buffer.readLine();
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public void processKillerPid(String os, int pid) {
+		if (os.contains("Linux")) {
+			StringBuffer cmd = new StringBuffer();
+			cmd.append("kill -9");
+			cmd.append(" ");
+			cmd.append(pid);
+			try {
+				Runtime.getRuntime().exec(cmd.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void processKillerName(String os, String name) {
+		if (os.contains("Linux")) {
+			StringBuffer cmd = new StringBuffer();
+			cmd.append("pkill");
+			cmd.append(" ");
+			cmd.append(name);
+			try {
+				Runtime.getRuntime().exec(cmd.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,9 +1,17 @@
 package view;
 
+import controller.ProcessController;
+
 public class Principal {
 
 	public static void main(String[] args) {
-
+		ProcessController pcs = new ProcessController();
+		
+//		System.out.println(os);
+//		String os = pcs.os();
+//		pcs.taskList(os);
+//		pcs.processKillerPid(os, 37915);
+//		pcs.processKillerName(os, "chrome");
 	}
 
 }
